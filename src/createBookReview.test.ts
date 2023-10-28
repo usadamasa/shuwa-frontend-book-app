32135d532804da154fa6e1ae98e64fb9bbfb41d1
usadamasa <usadamasa@gmail.com>
2023-10-28 20:40:29 +0900

    add test

--- src/createBookReview.test.ts
+++ src/createBookReview.test.ts
@@ -15,4 +15,19 @@ describe('createBookReview()', () => {
     document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`
     expect($('.review__list__item').length).toBe(1)
   })
+
+  test('should match username', () => {
+    document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`
+    expect($('.review__list__item__name').text()).toBe(`${review.username}さんの感想・評価`)
+  })
+
+  test('should match comment', () => {
+    document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`
+    expect($('.review__list__item__comment').text()).toBe(`${review.comment}`)
+  })
+
+  test('should match like count', () => {
+    document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`
+    expect($('.review__list__item__like__button').text()).toBe(`❤️ ${review.like}件`)
+  })
 })
