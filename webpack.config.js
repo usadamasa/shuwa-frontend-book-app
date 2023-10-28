2aac93d2301f56feccf3518e1195e7e58e689a81
usadamasa <usadamasa@gmail.com>
2023-10-28 19:42:07 +0900

    migrate ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './app.js',
+  entry: './app.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
