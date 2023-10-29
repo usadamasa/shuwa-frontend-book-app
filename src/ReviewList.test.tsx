a546808656761156ac413d1f2249f479c20ad1e1
usadamasa <usadamasa@gmail.com>
2023-10-29 11:25:09 +0900

    add test

---
+++ src/ReviewList.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {shallow} from "enzyme";
+
+import ReviewList from "./ReviewList";
+import {Review} from "@/app";
+
+describe('<ReviewList>', () => {
+  const review: Review = {
+    id: 1,
+    username: 'テストユーザー',
+    comment: 'この本はとても面白かったし学びも多い1冊でした。',
+    like: 3
+  }
+
+  test('should retrun <ul> elem', () => {
+    const wrapper = shallow(<ReviewList reviews={[review]}/>);
+    expect(wrapper.is('.review__list')).toBe(true);
+  });
+});
